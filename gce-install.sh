fece1ebeed95952f6f565d89638abefaeeb85188
axey733 <68095843+axey733@users.noreply.github.com>
2020-07-15 08:44:31 +0700

    updated repository source

--- gce-install.sh
+++ gce-install.sh
@@ -11,7 +11,8 @@ apt-get install -y libgbm-dev
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -yq git libgconf-2-4 nodejs
 
-git clone https://github.com/sahava/web-scraper-gcp.git
+#!git clone https://github.com/sahava/web-scraper-gcp.git
+git clone https://github.com/axey733/web-scraper-gcp.git
 
 cd web-scraper-gcp
 sudo npm install
